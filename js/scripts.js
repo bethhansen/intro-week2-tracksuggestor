931a0428f046772b1488cf60b2c9374b815c8d7e
Guest User <Guest@Epicodus-10C.local>
2017-07-07 11:17:19 -0700

    add the js functions and variables to return what the user has chosen

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
-var add = function(num1, num2, num3, num4, num5) {
-  return num1 + num2 + num3 + num4 + num5;
-};
-
+// var add = function(num1, num2, num3, num4, num5) {
+//   return num1 + num2 + num3 + num4 + num5;
+// };
+//
+//
+//
+// $(document).ready(function() {
+//   $("form.track-suggestor").submit(function(event)) {
+//     event.preventDefault();
+//     var question1 = parseInt($("#question1").val());
+//     var question2 = parseInt($("#question2").val());
+//     var question3 = parseInt($("#question3").val());
+//     var question4 = parseInt($("#question4").val());
+//     var question5 = parseInt($("#question5").val());
+//     var result = add(num1, num2, num3, num4, num5);
+//     $("#output").text(result);
+// });
 
 
 $(document).ready(function() {
-  $("form.track-suggestor").submit(function(event)) {
+  $("form#track-suggestor").submit(function(event){
+    var question1 = $("option.num1").val();
+    var question2 = $("option.num2").val();
+    var question3 = $("option.num3").val();
+    var question4 = $("option.num4").val();
+    var question5 = $("option.num5").val();
+
+    $(".num1").text(question1);
+    $(".num2").text(question2);
+    $(".num3").text(question3);
+    $(".num4").text(question4);
+    $(".num5").text(question5);
+
+    $("#result").show();
     event.preventDefault();
-    var question1 = parseInt($("#question1").val());
-    var question2 = parseInt($("#question2").val());
-    var question3 = parseInt($("#question3").val());
-    var question4 = parseInt($("#question4").val());
-    var question5 = parseInt($("#question5").val());
-    var result = add(num1, num2, num3, num4, num5);
-    $("#output").text(result);
-});
+      })
+})
