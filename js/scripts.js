b7e373228f6fb0cf1c601e433c36b4454ea869de
Beth <beth97209@gmail.com>
2017-07-09 13:52:39 -0700

    add updated additions to track choices

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var add = function(question1, question2) {
-  return question1 + question2
+var add = function(question1, question2, question3, question4, question5) {
+  return question1 + question2 + question3 + question4 + question5
 };
 
 $(document).ready(function() {
@@ -12,38 +12,95 @@ $(document).ready(function() {
 
     var result = add(question1, question2, question3, question4, question5);
 
-if (result === 1 || result <= 5) {
+if (result === 1 || result <= 10) {
   $(".suggestion2").show();
-  $(".druple").hide();
-  $(".html").hide();
+  $(".intro").show();
+  $(".html").show();
   $(".css").show();
   $(".c").hide();
   $(".java").hide();
   $(".javascript").hide();
   $(".android").hide();
-  $(".php").hide();
+  $(".design").show();
+  $(".ruby").hide();
+  $(".rails").hide();
+  $(".react").hide();
+  $(".net").hide(;)
 }
-else if (result === 6 || result <= 10) {
+else if (result === 11 || result <= 20) {
   $(".suggestion2").show();
-  $(".druple").hide();
+  $(".intro").hide();
+  $(".html").hide();
+  $(".css").show();
+  $(".c").hide();
+  $(".java").hide();
+  $(".javascript").show();
+  $(".android").hide();
+  $(".design").show();
+  $(".ruby").hide();
+  $(".rails").hide();
+  $(".react").hide();
+  $(".net").hide(;)
+}
+else if (result === 21 || result <= 30) {
+  $(".suggestion2").show();
+  $(".intro").show();
+  $(".html").hide();
+  $(".css").show();
+  $(".c").hide();
+  $(".java").hide();
+  $(".javascript").show();
+  $(".android").hide();
+  $(".ruby").hide();
+  $(".rails").hide();
+  $(".design").show();
+  $(".react").hide();
+  $(".net").hide(;)
+}
+else if (result === 31 || result <= 40) {
+  $(".suggestion2").show();
+  $(".intro").hide();
   $(".html").hide();
   $(".css").hide();
   $(".c").hide();
   $(".java").hide();
   $(".javascript").show();
   $(".android").hide();
-  $(".php").hide();
+  $(".design").hite();
+  $(".ruby").show();
+  $(".rails").show();
+  $(".react").hide();
+  $(".net").hide(;)
+}
+else if (result === 41 || result <= 50) {
+  $(".suggestion2").show();
+  $(".intro").hide();
+  $(".html").hide();
+  $(".css").hite();
+  $(".c").hide();
+  $(".java").show();
+  $(".javascript").show();
+  $(".android").show();
+  $(".design").hide();
+  $(".ruby").hide();
+  $(".rails").hide();
+  $(".react").hide();
+  $(".net").hide(;)
 }
 else {
-  $(".suggestion2").hide();
-  $(".druple").hide();
+  $(".suggestion2").show();
+  $(".intro").hide();
   $(".html").hide();
   $(".css").hide();
-  $(".c").hide();
+  $(".c").show();
   $(".java").hide();
-  $(".javascript").hide();
+  $(".javascript").show();
   $(".android").hide();
-  $(".php").hide();
+  $(".design").hide();
+  $(".ruby").hide();
+  $(".rails").hide();
+  $(".react").hide();
+  $(".net").show();
 }
   $("#result").show();//the string is now told to show its collected answers in the result area which has a unique ID
   event.preventDefault();//this tells the function to reset itself
