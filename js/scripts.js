5ae173081faf0e2ccff7ae79826872ef545de2cc
Beth <beth97209@gmail.com>
2017-07-12 21:21:54 -0700

    add working selectors

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var add = function(question1, question2, question3, question4, question5) {
 };
 
 $(document).ready(function() {
-  $("form#track-suggestor").submit(function(event) {
+  $("form#suggestor").submit(function(event) {
     var question1 = $("select#question1").val();
     var question2 = $("select#question2").val();
     var question3 = $("select#question3").val();
@@ -12,48 +12,48 @@ $(document).ready(function() {
     var result = add(question1, question2, question3, question4, question5);
 
 event.preventDefault();
-if (question1 === "a" && question2 === "a" question3 === "b") {
+if (question1 === "a" && question2 === "a" && question3 === "a" && question4 === "a" && question5 === "a") {
   $(".suggestion2").show();
-  $(".intro").hide();
-  $(".html").hide();
-  $(".css").hide();
-  $(".c").hide();
-  $(".java").hide();
-  $(".javascript").hide();
-  $(".android").show();
-  $(".design").hide();
-  $(".ruby").show();
-  $(".rails").hide();
-  $(".react").show();
-  $(".net").hide();
-}
-else if ( question3 === "b" && question4 === "b") {
-  $(".suggestion2").show();
-  $(".intro").hide();
-  $(".html").hide();
-  $(".css").hide();
-  $(".c").hide();
-  $(".java").hide();
-  $(".javascript").hide();
-  $(".android").show();
-  $(".design").show();
-  $(".ruby").hide();
-  $(".rails").hide();
-  $(".react").show();
-  $(".net").hide();
-}
-else {
-  $(".suggestion2").show();
-  $(".intro").hide();
+  $(".intro").show();
   $(".html").show();
   $(".css").show();
   $(".c").hide();
+  $(".java").hide();
+  $(".javascript").hide();
+  $(".android").hide();
+  $(".design").hide();
+  $(".ruby").hide();
+  $(".rails").hide();
+  $(".react").hide();
+  $(".net").hide();
+}
+else if (question1 === "b" && question2 === "b" && question3 === "b" && question4 === "b" && question5 === "b") {
+  $(".suggestion2").show();
+  $(".intro").hide();
+  $(".html").hide();
+  $(".css").hide();
+  $(".c").show();
   $(".java").show();
   $(".javascript").show();
   $(".android").hide();
   $(".design").hide();
   $(".ruby").hide();
   $(".rails").hide();
+  $(".react").hide();
+  $(".net").hide();
+}
+else {
+  $(".suggestion2").show();
+  $(".intro").hide();
+  $(".html").hide();
+  $(".css").hide();
+  $(".c").hide();
+  $(".java").hide();
+  $(".javascript").hide();
+  $(".android").hide();
+  $(".design").show();
+  $(".ruby").show();
+  $(".rails").show();
   $(".react").hide();
   $(".net").hide();
 }
