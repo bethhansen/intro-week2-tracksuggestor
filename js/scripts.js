1697e4cf628dd09c2823247eb2ff3b9f364dc577
beth97209@gmail.com <Guest@Epicodus-9B.local>
2017-07-17 13:02:27 -0700

    add correction to css js html

--- js/scripts.js
+++ js/scripts.js
@@ -12,51 +12,51 @@ $(document).ready(function() {
     var result = add(question1, question2, question3, question4, question5);
 
 event.preventDefault();
-if (question1 === "a" && question2 === "a" && question3 === "a" && question4 === "a" && question5 === "a") {
-  $(".suggestion2").show();
-  $(".intro").show();
-  $(".html").show();
-  $(".css").show();
-  $(".c").hide();
-  $(".java").hide();
-  $(".javascript").hide();
-  $(".android").hide();
-  $(".design").hide();
-  $(".ruby").hide();
-  $(".rails").hide();
-  $(".react").hide();
-  $(".net").hide();
-}
-else if (question1 === "b" && question2 === "b" && question3 === "b" && question4 === "b" && question5 === "b") {
-  $(".suggestion2").show();
-  $(".intro").hide();
-  $(".html").hide();
-  $(".css").hide();
-  $(".c").show();
-  $(".java").show();
-  $(".javascript").show();
-  $(".android").hide();
-  $(".design").hide();
-  $(".ruby").hide();
-  $(".rails").hide();
-  $(".react").hide();
-  $(".net").hide();
-}
-else {
-  $(".suggestion2").show();
-  $(".intro").hide();
-  $(".html").hide();
-  $(".css").hide();
-  $(".c").hide();
-  $(".java").hide();
-  $(".javascript").hide();
-  $(".android").hide();
-  $(".design").show();
-  $(".ruby").show();
-  $(".rails").show();
-  $(".react").hide();
-  $(".net").hide();
-}
-alert ("howdy");
+  if (question1 === "a" && question2 === "a" && question3 === "a" && question4 === "a" && question5 === "a") {
+    $(".suggestion2").show();
+    $(".intro").show();
+    $(".html").show();
+    $(".css").show();
+    $(".c").hide();
+    $(".java").hide();
+    $(".javascript").hide();
+    $(".android").hide();
+    $(".design").hide();
+    $(".ruby").hide();
+    $(".rails").hide();
+    $(".react").hide();
+    $(".net").hide();
+  }
+  else if (question1 === "b" && question2 === "b" && question3 === "b" && question4 === "b" && question5 === "b") {
+    $(".suggestion2").show();
+    $(".intro").hide();
+    $(".html").hide();
+    $(".css").hide();
+    $(".c").show();
+    $(".java").show();
+    $(".javascript").show();
+    $(".android").hide();
+    $(".design").hide();
+    $(".ruby").hide();
+    $(".rails").hide();
+    $(".react").hide();
+    $(".net").hide();
+  }
+  else {
+    $(".suggestion2").show();
+    $(".intro").hide();
+    $(".html").hide();
+    $(".css").hide();
+    $(".c").hide();
+    $(".java").hide();
+    $(".javascript").hide();
+    $(".android").hide();
+    $(".design").show();
+    $(".ruby").show();
+    $(".rails").show();
+    $(".react").hide();
+    $(".net").hide();
+  }
+  alert ("howdy");
   });
 });
